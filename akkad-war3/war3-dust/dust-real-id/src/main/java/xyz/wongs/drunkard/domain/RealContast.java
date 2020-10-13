e13bc7d6d0afba41e31a828364f71c7c11215d56
WONGS <WCNGS@QQ.COM>
2020-10-13 14:31:57 +0800

    Revised text format

--- akkad-war3/war3-dust/dust-real-id/src/main/java/xyz/wongs/drunkard/domain/RealContast.java
+++ akkad-war3/war3-dust/dust-real-id/src/main/java/xyz/wongs/drunkard/domain/RealContast.java
@@ -8,5 +8,5 @@ public class RealContast extends Constant {
     public static final String REAL_STATUS_N = "N";
     public static final String STATUS = "00";
 
-    public static final String REAL_URL = "http://crm3eop.sc.ctc.com/serviceAgent/http/1000020010/gztIdentityCheck";
+    public static final String REAL_URL = "http://crm3eop.ctc.com/serviceAgent/http/1000020010/gztIdentityCheck";
 }
